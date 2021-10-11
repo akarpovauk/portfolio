5663d8edf836f5337e6a60b69040a835fd72cc87
Alena <alena.karpov@gmail.com>
2021-10-11 14:33:46 +0100

    media for cv and experience sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,31 +1,14 @@
 const   hamburger = document.querySelector('.hamburger'),
         menu = document.querySelector('.menu'),
-        menuLink = document.querySelectorAll('.menu__link'),
         closeElem = document.querySelector('.menu__close');
-       
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
-/* закрытие при нажатии на Esc */
-window.addEventListener("keydown", (e) => {
-    if (e.key == 'Escape') {
-        menu.classList.remove('active');
-    }
-});
-
-
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
-
-menuLink.forEach((item, i) => {
-    menuLink[i].addEventListener('click', () => {
-        menu.classList.remove('active');
-    });
-});
-
 
 const   counters = document.querySelectorAll('.progress__value'),
         bars = document.querySelectorAll('.progress__bar_filled');
