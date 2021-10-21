e24e394f2b68ae615a669ba6e808084d7e1313ca
Alena <alena.karpov@gmail.com>
2021-10-21 14:46:16 +0100

    js - pageup, media, policy-media, hover effects

--- src/js/script.js
+++ src/js/script.js
@@ -88,4 +88,21 @@ $(document).ready(function(){
 
         return false;
     });
+
+    //smlooth scroll and page up
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    
 });
